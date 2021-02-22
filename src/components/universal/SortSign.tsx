c5d61d28bc2c27d174601ad27b82b6b5848f8cec
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-02-22 17:20:41 +0300

    Добавляем переключатель.

--- src/components/universal/SortSign.tsx
+++ src/components/universal/SortSign.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+
+const style = {cursor: 'pointer'};
+
 export default function SortSign() {
   
+  let [isUp, setUp] = React.useState(false);
+
+  const onClick = React.useCallback(() => {
+    setUp((prevVal: boolean) => {return !prevVal;})
+  }, [setUp]);
+
     return (
-      <div>
-         Привет
+      <div onClick={onClick} style={style}>
+        {isUp ? 
+         <p>Вниз</p> : <p>Вверх</p>}
       </div>
     );
 }
