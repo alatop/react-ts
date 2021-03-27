e5286509412d9c708741faf6ba2a3559585c4551
vedro-compota <ru-ibn-hottab@yandex.ru>
2021-03-27 22:46:59 +0300

    Добавлен сброс списка городов при выборе страны.

--- src/reducers/RootReducer.ts
+++ src/reducers/RootReducer.ts
@@ -87,6 +87,18 @@ export default function rootReducer(state = initialState, action: commonActionTy
         formDataSaved: false,
       }
     case RootActionTypes.EDIT_FORM_GOODS_ITEM_DATA_VALUE:
+      if ((action.data.name == 'country')
+        && state.formData.country && (action.data.value !== state.formData.country)) {
+          return {
+            ...state,
+            formData: { 
+              ...state.formData,
+              [action.data.name]: action.data.value,
+              cities: [],
+            },
+            
+          }
+      } else 
       return {
         ...state,
         formData: { ...state.formData, [action.data.name]: action.data.value },
